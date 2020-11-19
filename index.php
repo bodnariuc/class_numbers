0d77f8229d4d945bfc69317bbc93133f3349fa3c
bodnariuc <62306413+bodnariuc@users.noreply.github.com>
2020-11-19 17:27:21 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+include("class_numbers.php");
+
+var_dump(NumbersLukasBodnariuc::roundLukasBodnariuc(1.0295, 3));
+
+var_dump(NumbersLukasBodnariuc::ceilLukasBodnariuc(6.9));
+
+var_dump(NumbersLukasBodnariuc::floorLukasBodnariuc(5.7));
+
+var_dump(NumbersLukasBodnariuc::sqrtLukasBodnariuc(31));
+
+var_dump(NumbersLukasBodnariuc::powLukasBodnariuc(4, 3));
+
+var_dump(NumbersLukasBodnariuc::sinLukasBodnariuc(0));
+
+var_dump(NumbersLukasBodnariuc::cosLukasBodnariuc(90));
+
+var_dump(NumbersLukasBodnariuc::tanLukasBodnariuc(45));
+
+var_dump(NumbersLukasBodnariuc::asinLukasBodnariuc(45));
+
+var_dump(NumbersLukasBodnariuc::acosLukasBodnariuc(180));
+
+var_dump(NumbersLukasBodnariuc::atanLukasBodnariuc(360));
+
+var_dump(NumbersLukasBodnariuc::fmodLukasBodnariuc(12.1, 12.0));
+
+var_dump(NumbersLukasBodnariuc::maxLukasBodnariuc([10, 2, 16, 8, 4]));
+
+var_dump(NumbersLukasBodnariuc::minLukasBodnariuc([15, 14, 16, 8, 159]));
+
+var_dump(NumbersLukasBodnariuc::absLukasBodnariuc(-10));
+
+var_dump(NumbersLukasBodnariuc::subtrLukasBodnariuc(15, 10));
+
+var_dump(NumbersLukasBodnariuc::addLukasBodnariuc(33, 36));
+
+var_dump(NumbersLukasBodnariuc::divLukasBodnariuc(155, 4));
+
+var_dump(NumbersLukasBodnariuc::multiLukasBodnariuc(16, 12.5));
+
+var_dump(NumbersLukasBodnariuc::number_formatLukasBodnariuc(45154.12445, 2, "¤", ","));
+
+?>
