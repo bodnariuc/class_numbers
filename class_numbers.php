02a0fb3b6f457b3d7bccffb58981ffefd55b659a
bodnariuc <62306413+bodnariuc@users.noreply.github.com>
2020-11-19 17:26:56 +0100

    Update class_numbers.php

--- class_numbers.php
+++ class_numbers.php
@@ -1,131 +1,231 @@
 <?php
 
-class NumbersBodnariuc {
-    
-    public static function roundBodnariuc() {
-        $value = 3.265;
-        return round($value);
-    }
-    
-    public static function ceilBodnariuc() {
-         $value = 3.2;
-        return ceil($value);
-    }
-    
-    public static function floorBodnariuc() {
-        $value = 3.8;
-        return floor($value);
-    }
-    
-    public static function sqrtBodnariuc() {
-        $value = 4;
-        return sqrt($value);
-    }
-    
-    public static function powBodnariuc() {
-        $zaklad = 2;
-        $mocnitel = 3;
-        return pow($zaklad, $mocnitel);
-    }
-    
-    public static function sinBodnariuc() {
-        $value = 45;
-        return sin($value);
-    }
+declare(strict_types = 1);
 
-    public static function cosBodnariuc() {
-        $value = 45;
-        return cos($value);
-    }
-
-    public static function tanBodnariuc() {
-        $value = 45;
-        return tan($value);
+class NumbersLukasBodnariuc{
+    
+    /**
+     * 
+     * @param float $cislo
+     * @param type $presnost
+     * @return type
+     */
+    public static function roundLukasBodnariuc(float $cislo, $presnost = NULL) {
+        
+        if (is_null($presnost)) {
+            return round($cislo);
+        }
+        else {
+            return round($cislo, $presnost);
+        }
     }
     
-    public static function asinBodnariuc() {
-        $value = 45;
-        return asin($value);
-    }
-
-    public static function acosBodnariuc() {
-        $value = 45;
-        return acos($value);
-    }
- 
-    public static function atanBodnariuc() {
-        $value = 45;
-        return atan($value);
+    /**
+     * 
+     * @param type $cislo
+     * @return type
+     */
+    public static function ceilLukasBodnariuc($cislo) {
+        
+        return ceil($cislo);
     }
     
-    public static function fmodBodnariuc() {
-        $x = 15;
-        $y = 6;
-        return fmod($x, $y);
+    /**
+     * 
+     * @param type $cislo
+     * @return type
+     */
+    public static function floorLukasBodnariuc($cislo) {
+        
+        return floor($cislo);
     }
     
-    public static function maxBodnariuc() {
-        $values = array('1', '2', '3');
-        return max($values);
-    }
-
-    public static function minBodnariuc() {
-        $values = array('1', '2', '3');
-        return min($values);
-    }
-    
-    public static function absBodnariuc() {
-        $value = -15;
-        return abs($value);
+    /**
+     * 
+     * @param type $cislo
+     * @return type
+     */
+    public static function sqrtLukasBodnariuc($cislo) {
+        
+        return sqrt($cislo);
     }
     
-    public static function minusBodnariuc() {
-        $a = 1;
-        $b = 2;
-        return ($a - $b);
-    }
-
-    public static function plusBodnariuc() {
-        $a = 1;
-        $b = 2;
-        return ($a + $b);
-    }
-
-    public static function delenoBodnariuc() {
-        $a = 1;
-        $b = 2;
-        return ($a / $b);
-    }
-
-    public static function kratBodnariuc() {
-        $a = 1;
-        $b = 2;
-        return ($a * $b);
+    /**
+     * 
+     * @param type $cislo
+     * @param type $exponent
+     * @return type
+     */
+    public static function powLukasBodnariuc($cislo, $exponent) {
+        
+        return pow($cislo, $exponent);
     }
     
-    public static function number_formatBodnariuc() {
-        $value = 10;
-        return number_format($value);
+    /**
+     * 
+     * @param float $cislo
+     * @return float
+     */
+    public static function sinLukasBodnariuc(float $cislo): float {
+        
+        return sin($cislo);
     }
+    
+    /**
+     * 
+     * @param float $cislo
+     * @return float
+     */
+    public static function cosLukasBodnariuc(float $cislo): float {
+        
+        return cos($cislo);
+    }
+    
+    /**
+     * 
+     * @param float $cislo
+     * @return float
+     */
+    public static function tanLukasBodnariuc(float $cislo): float {
+        
+        return tan($cislo);
+    }
+    
+    /**
+     * 
+     * @param float $cislo
+     * @return float
+     */
+    public static function asinLukasBodnariuc(float $cislo): float {
+        
+        return asin($cislo);
+    }
+    
+    /**
+     * 
+     * @param float $cislo
+     * @return float
+     */
+    public static function acosLukasBodnariuc(float $cislo): float {
+        
+        return acos($cislo);
+    }
+    
+    /**
+     * 
+     * @param float $cislo
+     * @return float
+     */
+    public static function atanLukasBodnariuc(float $cislo): float {
+        
+        return atan($cislo);
+    }
+    
+    /**
+     * 
+     * @param float $cislo1
+     * @param float $cislo2
+     * @return float
+     */
+    public static function fmodLukasBodnariuc(float $cislo1, float $cislo2): float {
+        
+        return fmod($cislo1, $cislo2);
+    }
+    
+    /**
+     * 
+     * @param array $cisla
+     * @return type
+     */
+    public static function maxLukasBodnariuc(array $cisla) {
+        
+        return max($cisla);
+    }
+    
+    /**
+     * 
+     * @param array $cisla
+     * @return type
+     */
+    public static function minLukasBodnariuc(array $cisla) {
+        
+        return min($cisla);
+    }
+    
+    /**
+     * 
+     * @param type $cislo
+     * @return type
+     */
+    public static function absLukasBodnariuc($cislo) {
+        
+        return abs($cislo);
+    }
+    
+    /**
+     * 
+     * @param type $cislo1
+     * @param type $cislo2
+     * @return type
+     */
+    public static function subtrLukasBodnariuc($cislo1, $cislo2) {
+        
+        return ($cislo1 - $cislo2);
+    }
+    
+    /**
+     * 
+     * @param type $cislo1
+     * @param type $cislo2
+     * @return type
+     */
+    public static function addLukasBodnariuc($cislo1, $cislo2) {
+        
+        return ($cislo1 + $cislo2);
+    }
+    
+    /**
+     * 
+     * @param type $cislo1
+     * @param type $cislo2
+     * @return type
+     */
+    public static function divLukasBodnariuc($cislo1, $cislo2) {
+        
+        return ($cislo1 / $cislo2);
+    }
+    
+    /**
+     * 
+     * @param type $cislo1
+     * @param type $cislo2
+     * @return type
+     */
+    public static function multiLukasBodnariuc($cislo1, $cislo2) {
+        
+        return ($cislo1 * $cislo2);
+    }
+    
+    /**
+     * 
+     * @param float $cislo
+     * @param type $decimal
+     * @param type $znamenko
+     * @param type $oddelovac
+     * @return string
+     */
+    public static function number_formatLukasBodnariuc(float $cislo, $decimal = NULL, $znamenko = NULL, $oddelovac = NULL): string {
+        
+        if (!empty($decimal) && is_null($znamenko) && is_null($oddelovac)){
+            return number_format($cislo, $decimal);
+        }
+        else if (!empty($decimal) && !empty($znamenko) && !empty($oddelovac)){
+            return number_format($cislo, $decimal, $znamenko, $oddelovac);
+        }
+        else {
+            return ($cislo);
+        }
+    } 
 }
-var_dump(NumbersBodnariuc::roundBodnariuc());
-var_dump(NumbersBodnariuc::ceilBodnariuc());
-var_dump(NumbersBodnariuc::floorBodnariuc());
-var_dump(NumbersBodnariuc::sqrtBodnariuc());
-var_dump(NumbersBodnariuc::powBodnariuc());
-var_dump(NumbersBodnariuc::sinBodnariuc());
-var_dump(NumbersBodnariuc::cosBodnariuc());
-var_dump(NumbersBodnariuc::tanBodnariuc());
-var_dump(NumbersBodnariuc::asinBodnariuc());
-var_dump(NumbersBodnariuc::acosBodnariuc());
-var_dump(NumbersBodnariuc::atanBodnariuc());
-var_dump(NumbersBodnariuc::fmodBodnariuc());
-var_dump(NumbersBodnariuc::maxBodnariuc());
-var_dump(NumbersBodnariuc::minBodnariuc());
-var_dump(NumbersBodnariuc::absBodnariuc());
-var_dump(NumbersBodnariuc::minusBodnariuc());
-var_dump(NumbersBodnariuc::plusBodnariuc());
-var_dump(NumbersBodnariuc::delenoBodnariuc());
-var_dump(NumbersBodnariuc::kratBodnariuc());
-var_dump(NumbersBodnariuc::number_formatBodnariuc());
+
 ?>
